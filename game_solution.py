1c559062fdb5fbfa37b13a8f92fcb37572c5acbb
Antony <antony.a.salta@gmail.com>
2023-11-11 20:54:27 +0000

    Made it so that the player only moves when key is held, and mobs will stay inside the canvas

--- game_solution.py
+++ game_solution.py
@@ -8,30 +8,26 @@ from mobiles import Player
 def doSomething():
     print("something")
 
-def moveLeft(event):
-    global player, window
-    player.xSpeed = -5
-    print(player)
-    print("left detected" + str(player.xSpeed))
-    #window.after(5000,doSomething)
-    
-def moveRight(event):
+def moveLeft(*ignore):
     global player
-    player.xSpeed = 3
+    player.changeXSpeed(-player.speed)
+    print("left detected")
+def moveRight(*ignore):
+    global player
+    player.changeXSpeed(player.speed)
     print("right detected")
-def moveUp(event):
+def moveUp(*ignore):
     global player
-    player.ySpeed = -3
+    player.changeYSpeed(-player.speed)
     print("up detected")
-def moveDown(event):
+def moveDown(*ignore):
     global player
-    player.ySpeed = 3
+    player.changeYSpeed(player.speed)
     print("down detected")
 
 def moveMobiles():
     global mobiles
     for mob in mobiles:
-        print("in the move loop")
         mob.move()
         
 
@@ -39,13 +35,12 @@ def update(*ignore):
     global window
     if not paused:
         moveMobiles()
-        print("in update")
         window.after(10,update)
 
-def moveCrab(*ignore):
-    global crabID
-    global canvas
-    canvas.moveto(crabID, 400, 400)
+def fire(event):
+    global player
+    shotID = canvas.create_oval(player.x+10,player.y+10,player.x+20,player.y+20,fill="yellow")
+
         
 
 window = Tk()
@@ -55,21 +50,28 @@ test = Image.open("crab.jpg") # crab found here https://pixabay.com/photos/crab-
 test = test.resize((500,200), Image.LANCZOS) # Not sure how needed LANCZOS is needed, but it's some form of antialias.
 test = ImageTk.PhotoImage(test) # Have to convert to PhotoImage to use in the canvas
 canvas.pack()
+print(canvas.winfo_height())
+print(canvas.winfo_width())
 crabID = canvas.create_image(20,20,anchor="nw",image=test) # anchor basically says to take a certain part of an image, a corner, edge or center, and make that part of the image appear at the specified coordinates
 #coordinates at the beginning are x then y
-
+canvas.create_oval(520,520,530,530,fill="yellow")
 mobiles=[]
 paused = False
 temp = PhotoImage(file="assets/player/player.png") # for some reason I can't just pass it into rhe create_image method
 playerImageID = canvas.create_image(766,800,anchor="nw",image= temp)
-player = Player(760,800,playerImageID,canvas)
+player = Player(766,800,playerImageID,canvas, temp.height(), temp.width())
 mobiles.append(player)
+
 
 window.bind("a",moveLeft)
 window.bind("d",moveRight)
 window.bind("s",moveDown)
 window.bind("w",moveUp)
+window.bind("<KeyRelease-a>",moveRight)
+window.bind("<KeyRelease-d>",moveLeft)
+window.bind("<KeyRelease-s>",moveUp)
+window.bind("<KeyRelease-w>",moveDown)
+window.bind("<Button-1>",fire)
 window.bind("p",update)
-window.bind("m",moveCrab)
 
 window.mainloop()
--- mobiles.py
+++ mobiles.py
@@ -1,14 +1,26 @@
 import math
 from tkinter import PhotoImage, Canvas
 
+def keepInBounds(num,min,max):
+        if num < min:
+            num = min
+        elif num > max:
+            num = max
+        return num
+
 class Mobile:
-    def __init__(self, x, y, imageID, canvas) -> None:
+    def __init__(self, x, y, imageID, canvas, height, width, speed = 10) -> None:
         self.x =y
         self.y =y
         self.imageID = imageID
         self.canvas = canvas
         self.xSpeed =0
         self.ySpeed=0
+        self.speed = speed
+        self.height = height
+        self.width = width
+    
+    
     
     def move(self):
         """This will update the position of the object.
@@ -18,17 +30,27 @@ class Mobile:
         """
         if self.xSpeed != 0 or self.ySpeed != 0: 
             self.x += self.xSpeed
+            self.x = keepInBounds(self.x,0,self.canvas.winfo_width() - self.width)
+
             self.y += self.ySpeed
-            self.canvas.move(self.imageID, self.xSpeed, self.ySpeed)
-            print(str(self.x) + ", " + str(self.y)) 
+            self.y = keepInBounds(self.y,0,self.canvas.winfo_height() -self.height)
+            self.canvas.moveto(self.imageID, math.floor(self.x), math.floor(self.y)) # Could use move, but I prefer this, it could help if things move at weird angles.
+            
+    def changeXSpeed(self,change):
+        self.xSpeed += change
+        self.xSpeed = keepInBounds(self.xSpeed,-self.speed,self.speed)
+    def changeYSpeed(self,change):
+        self.ySpeed += change
+        self.ySpeed = keepInBounds(self.ySpeed,-self.speed,self.speed)
     
 
 class Player(Mobile):
     """This is the player class, which the user will control in this space fighter game.
     """
-    def __init__(self, x,y, image, canvas, health =3,) -> None:
-        super().__init__(x,y,image, canvas)
-        # self.x =y
-        # self.x =y
-        # self.image = image
+    def __init__(self, x,y, image, canvas, height, width, health =3, speed =5) -> None:
+        super().__init__(x,y,image, canvas, height, width)
         self.health =health
+        self.speed = speed
+        
+    
+    
